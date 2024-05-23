2e9123b1b84a900d071d7fc75c8286d692ce6148
teixeira007 <viniciusteixeiravini16@gmail.com>
2024-05-23 14:57:18 -0300

    feat: decifrando para mais de 1 repetição

--- blocos_2.py
+++ blocos_2.py
@@ -42,46 +42,47 @@ def xor_de_bloco(bloco, bloco_auxiliar):
     return resultado_xor
 
 
-def l_r(left_block, right_block, aux, random_numbers_X, random_numbers_B):
+def l_r(left_block, right_block, aux, random_numbers_X, random_numbers_B, lista_linhas, lista_colunas):
     right_block = xor_de_bloco(right_block, aux)
-    # left_block = xor_de_bloco(left_block, bloco_teste)
     left_block, linhas, colunas = function_f.main(left_block, random_numbers_X, random_numbers_B)
+    lista_linhas.append(linhas)
+    lista_colunas.append(colunas)
     processed_image = np.hstack((right_block, left_block))
-    return processed_image, linhas, colunas
+    return processed_image, lista_linhas, lista_colunas
 
 def l_r_reverse(left_block, right_block, aux, random_numbers_X, random_numbers_B, linhas, colunas):
             left_block  = xor_de_bloco(left_block, aux)
-            # left_block = xor_de_bloco(left_block, bloco_teste)
             right_block = function_f.decript(right_block, random_numbers_X, random_numbers_B, linhas, colunas)
             processed_image = np.hstack((right_block, left_block))
             return processed_image
 
-bloco_teste = criar_bloco_auxiliar((512,256,3), semente=43234)
 
 def main(imagem):# Carregar a imagem
     # Dividir a imagem em blocos
     left_block, right_block  = dividir_em_blocos(imagem)
-
     dimensao = left_block.shape
 
     semente = 42
+    l_next = imagem
+    random_numbers_X = beach.keygen(000.1, 3.915, 3)
 
-    random_numbers_X = beach.keygen(000.1, 3.915, 7)
-
-    random_numbers_B = beach.keygen(00.153, 3.915, 7)
+    random_numbers_B = beach.keygen(00.153, 3.915, 3)
 
     bloco_auxiliar_L = criar_bloco_auxiliar(dimensao, semente=semente)
-
+    lista_linhas = []
+    lista_colunas = []
     
-    for i in range (1):
-        l_next, linhas, colunas = l_r(left_block, right_block, bloco_auxiliar_L, random_numbers_X[i], random_numbers_B[i])
+    for i in range (3):
+        left_block, right_block  = dividir_em_blocos(l_next)
+
+        l_next, linhas, colunas = l_r(left_block, right_block, bloco_auxiliar_L, random_numbers_X[i], random_numbers_B[i], lista_linhas, lista_colunas)
 
 
 
     return l_next, linhas, colunas
         
 
-def decrypt(imagem, linhas, colunas):
+def decrypt(imagem, lista_linhas, lista_colunas):
     height, width, channels = imagem.shape
     mid = width // 2
     left_block = imagem[:, :mid, :]
@@ -91,18 +92,23 @@ def decrypt(imagem, linhas, colunas):
 
     semente = 42
 
-    random_numbers_X = beach.keygen(000.1, 3.915, 7)
+    random_numbers_X = beach.keygen(000.1, 3.915, 3)
 
-    random_numbers_B = beach.keygen(00.153, 3.915, 7)
+    random_numbers_B = beach.keygen(00.153, 3.915, 3)
 
     bloco_auxiliar_L = criar_bloco_auxiliar(dimensao, semente=semente)
-
+    l_next = imagem
     # Inverter a ordem das chaves para decriptação
-    # random_numbers_X = random_numbers_X[::-1]
-    # random_numbers_B = random_numbers_B[::-1]
+    random_numbers_X = random_numbers_X[::-1]
+    random_numbers_B = random_numbers_B[::-1]
 
-    for i in range(1):
-        l_next = l_r_reverse(left_block, right_block, bloco_auxiliar_L, random_numbers_X[i], random_numbers_B[i], linhas, colunas)
+    lista_linhas = lista_linhas[::-1]
+    lista_colunas = lista_colunas[::-1]
+
+    for i in range(3):
+        left_block, right_block  = dividir_em_blocos(l_next)
+
+        l_next = l_r_reverse(left_block, right_block, bloco_auxiliar_L, random_numbers_X[i], random_numbers_B[i], lista_linhas[i], lista_colunas[i])
 
     return l_next
 
--- main.py
+++ main.py
@@ -7,35 +7,15 @@ import function_f
 
 imagem = cv2.imread('lena.png')
 
-# image_cript, linhas, colunas = blocos_2.main(imagem)
+image_cript, linhas, colunas = blocos_2.main(imagem)
+plt.imshow(image_cript)
+plt.show()
 
-# altura, largura = imagem.shape[:2]
-# metade_altura = altura // 2
-# metade_largura = largura // 2
-left_block, right_block,  = blocos_2.dividir_em_blocos(imagem)
-
-bloco_auxiliar_L = blocos_2.criar_bloco_auxiliar(right_block.shape, semente=45)
-right_block  = blocos_2.xor_de_bloco(right_block, bloco_auxiliar_L)
-
-
-left_block, linhas, colunas = function_f.main(left_block, 0.003, 0.983)
-processed_image = np.hstack((right_block, left_block))
-
-
-plt.imshow(processed_image)
+image_decript = blocos_2.decrypt(image_cript, linhas, colunas)
+plt.imshow(image_decript)
 plt.show()
 
 
-
-
-left_block, right_block,  = blocos_2.dividir_em_blocos(processed_image)
-left_block  = blocos_2.xor_de_bloco(left_block, bloco_auxiliar_L)
-
-right_block = function_f.decript(right_block, 0.003, 0.983, linhas, colunas)
-imagem_ = np.hstack((right_block, left_block))
-# image_decript = blocos_2.decrypt(imagem, linhas, colunas)
-plt.imshow(imagem_)
-plt.show()
 
 
 
