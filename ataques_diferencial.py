85383e4824ae89c9faaf9739af97aaf2b188bc93
teixeira007 <viniciusteixeiravini16@gmail.com>
2024-05-27 11:36:34 -0300

    fix: ataques diferencial

--- ataques_diferencial.py
+++ ataques_diferencial.py
@@ -1,24 +1,37 @@
 import cv2
 
 def uaci(original, cifrada):
-    height, width = original.shape
-    value=0
-    for y in range(height):
-        for x in range(width):
-            value +=(abs(int(original[x,y])-int(cifrada[x,y])))
-    value = value*100/(width*height*255)
+    # Assumimos que a imagem é RGB com três canais.
+    height, width, channels = original.shape
+    value = 0
+    
+    for c in range(channels):
+        for y in range(height):
+            for x in range(width):
+                value += abs(int(original[y, x, c]) - int(cifrada[y, x, c]))
+    
+    # Calculamos o valor médio para todos os canais
+    value = value * 100 / (width * height * 255 * channels)
     return value
 
-def nrpc(imagem_original, imagem_modificada):
-    total_pixels = imagem_original.shape[0] * imagem_original.shape[1]
-    pixels_alterados = cv2.countNonZero(cv2.absdiff(imagem_original, imagem_modificada))
-    nrpc_value = pixels_alterados / total_pixels * 100
-    return nrpc_value
+
+def npcr(imagem_original, imagem_modificada):
+    # Assumimos que a imagem é RGB com três canais.
+    height, width, channels = imagem_original.shape
+    total_pixels = height * width * channels
+    pixels_alterados = 0
+    
+    for c in range(channels):
+        pixels_alterados += cv2.countNonZero(cv2.absdiff(imagem_original[:, :, c], imagem_modificada[:, :, c]))
+    
+    npcr_value = pixels_alterados / total_pixels * 100
+    return npcr_value
 
 
-enc_original = cv2.imread('imagens/sensibilidade_a_chave/img_cifrada_chave_0.001985_0.153972.bmp', cv2.IMREAD_GRAYSCALE)
-enc_cifrada = cv2.imread('imagens/sensibilidade_a_chave/img_cifrada_chave_totalmente_diferente.bmp', cv2.IMREAD_GRAYSCALE)
-print('UACI: ', uaci(enc_original, enc_cifrada))
+enc_original = cv2.imread('imagens/img_cifrada_r_3.99.bmp')
+enc_cifrada = cv2.imread('imagens/img_cifrada_1px_modificada.bmp')
+resultado_uaci  = uaci(enc_original, enc_cifrada)
+print('UACI: ', resultado_uaci)
 
-resultado_nrpc = nrpc(enc_original, enc_cifrada)
+resultado_nrpc = npcr(enc_original, enc_cifrada)
 print("NRPC: {:.2f}%".format(resultado_nrpc))
