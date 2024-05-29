64e5406357c216cc78d3ed6feb6232b1767cb498
teixeira007 <viniciusteixeiravini16@gmail.com>
2024-05-29 10:00:48 -0300

    testes nist

--- converter_imagem_para_bits.py
+++ converter_imagem_para_bits.py
@@ -1,12 +1,12 @@
 import cv2
 
 def image_to_bits(image_path):
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(image_path)
     bits = ''.join(format(byte, '08b') for byte in image.flatten())
     return bits
 
 # Exemplo de uso
-encrypted_image_path = 'imagens/lena_cifrada.bmp'
+encrypted_image_path = 'img_cifrada_chave_0.1987_0.73974_76453454.0_534675657.0.bmp'
 bit_sequence = image_to_bits(encrypted_image_path)
 
 # Salvar a sequência de bits em um arquivo
