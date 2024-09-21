75804207b0ab9cdbd7ecde98cb6505efaf5deefd
teixeira007 <viniciusteixeiravini16@gmail.com>
2024-09-21 12:35:56 -0300

    refatoração

--- alterar_1px__imagem.py
+++ alterar_1px__imagem.py
@@ -8,8 +8,8 @@ imagem_cinza = cv2.imread('imagens/imagens_para_teste/lena_gray_512.tif', cv2.IM
 altered_image = imagem_cinza.copy()
 altered_image[0,0] = (altered_image[0,0]+1)%256
 
-numbers = [0.0943063, 0.1987654, 0.7397466, 0.0098324, 0.0032313]
+numbers = [0.534234, 0.1987654, 0.23486, 0.973455, 0.563413]
 
 image_cript = blocos_2.main(altered_image, numbers)
-cv2.imwrite("lena_cifrado_1pixel.bmp" , image_cript)
+cv2.imwrite("lena_cifrado_1pixel__.bmp" , image_cript)
 
